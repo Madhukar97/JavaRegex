6af4fcd7988b9dac5e14123a8c9cc0a7c8d5c857
unknown <madhukar.tatiraju@gmail.com>
2021-11-10 22:56:17 +0530

    UC10 added

--- src/main/java/Password.java
+++ src/main/java/Password.java
@@ -3,6 +3,40 @@ import java.util.regex.Pattern;
 
 public class Password {
     /**
+     * This method takes First Name which starts with Capital letter and has minimum of 3 characters
+     *
+     * @param Firstname is used for comparing with regex
+     */
+    public boolean isFirstName(String Firstname) {
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}");
+        Matcher matcher = pattern.matcher(Firstname);
+        return matcher.matches();
+    }
+
+    /**
+     * This method takes Last Name which starts with Capital letter and has minimum of 3 characters
+     *
+     * @param Lastname is used for comparing with regex
+     */
+    public boolean isLastName(String Lastname) {
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}");
+        Matcher matcher = pattern.matcher(Lastname);
+        return matcher.matches();
+    }
+
+    /**
+     * This method checks if the entered number is valid or not
+     *
+     * @param Number takes in the parameter as String representation of number
+     * @return it returns a boolean value true if the number is valid
+     */
+    public boolean isNumber(String Number) {
+        Pattern pattern = Pattern.compile("^[0-9]{2}[ ][1-9][0-9]{9}$");
+        Matcher matcher = pattern.matcher(Number);
+        return matcher.matches();
+    }
+
+    /**
      * Method to check if the password is valid or not
      *
      * @param passcode takes in the passcode string as a parameter
@@ -18,6 +52,9 @@ public class Password {
     public static void main(String[] args) {
 
         Password obj = new Password();
+        System.out.println(obj.isFirstName("Madhukar"));
+        System.out.println(obj.isLastName("Tatiraju"));
+        System.out.println(obj.isNumber("91 9346546112"));
         System.out.println(obj.password("Madhukar@yg9j"));
     }
 }
