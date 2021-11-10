cac566acb4816bb6daebc52e0cbdbbeeb2a68352
unknown <madhukar.tatiraju@gmail.com>
2021-11-10 23:10:16 +0530

    UC11 added

---
+++ src/test/java/EmailValidationTest.java
@@ -0,0 +1,45 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class EmailValidationTest {
+    private final String emailId;
+    private final boolean expectedResult;
+    private Email email;
+
+    @Before
+    public void initializeClass() {
+        email = new Email();
+    }
+
+    public EmailValidationTest(String emailId, boolean expected) {
+        this.emailId = emailId;
+        this.expectedResult = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object> emailIds() {
+        return Arrays.asList(new Object[][]{
+                {"abc@yahoo.com", true}, {"abc-100@yahoo.com", true},
+                {"abc.100@yahoo.com", true}, {"abc111@yahoo.com", true},
+                {"abc-100@abc.net", true}, {"abc.100@abc.com.au", true},
+                {"abc@1.com", true}, {"abc@yahoo.com.com", true},
+                {"abc+100@yahoo.com", true}, {"abc", false}, {"abc@.com.my", false},
+                {"abc123@gmail.a", false}, {"abc123@.com", false}, {"abc123@.com.com", false},
+                {".abc@abc.com", false}, {"abc()*@gmail.com", false}, {"abc@%*.com", false},
+                {"abc..2002@gmail.com", false}, {"abc.@gmail.com", false}, {"abc@abc@gmail.com", false},
+                {"abc@gmail.com.1a", false}, {"abc@gmail.com.aa.au", false}
+        });
+    }
+
+    @Test
+    public void testEmailChecker() {
+        Assert.assertEquals(expectedResult, email.isEmail(emailId));
+    }
+}
