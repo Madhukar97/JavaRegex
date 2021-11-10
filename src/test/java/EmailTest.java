8d834691af5a58a3cab622fe9594d6659a1c7af6
unknown <madhukar.tatiraju@gmail.com>
2021-11-10 22:21:23 +0530

    UC9 added

--- src/test/java/EmailTest.java
+++ src/test/java/EmailTest.java
@@ -50,4 +50,68 @@ public class EmailTest {
         Assert.assertTrue(obj.isEmail("abc+100@yahoo.com"));
     }
 
+    @Test
+    public void isEmail10() {
+        Assert.assertFalse(obj.isEmail("abc"));
+    }
+
+    @Test
+    public void isEmail11() {
+        Assert.assertFalse(obj.isEmail("abc@.com.my"));
+    }
+
+    @Test
+    public void isEmail12() {
+        Assert.assertFalse(obj.isEmail("abc123@gmail.a"));
+    }
+
+    @Test
+    public void isEmail13() {
+        Assert.assertFalse(obj.isEmail("abc123@.com"));
+    }
+
+    @Test
+    public void isEmail14() {
+        Assert.assertFalse(obj.isEmail("abc123@.com.com"));
+    }
+
+    @Test
+    public void isEmail15() {
+        Assert.assertFalse(obj.isEmail(".abc@abc.com"));
+    }
+
+    @Test
+    public void isEmail16() {
+        Assert.assertFalse(obj.isEmail("abc()*@gmail.com"));
+    }
+
+    @Test
+    public void isEmail17() {
+        Assert.assertFalse(obj.isEmail("abc@%*.com"));
+    }
+
+    @Test
+    public void isEmail18() {
+        Assert.assertFalse(obj.isEmail("abc..2002@gmail.com"));
+    }
+
+    @Test
+    public void isEmail19() {
+        Assert.assertFalse(obj.isEmail("abc.@gmail.com"));
+    }
+
+    @Test
+    public void isEmail20() {
+        Assert.assertFalse(obj.isEmail("abc@abc@gmail.com"));
+    }
+
+    @Test
+    public void isEmail21() {
+        Assert.assertFalse(obj.isEmail("abc@gmail.com.1a"));
+    }
+
+    @Test
+    public void isEmail22() {
+        Assert.assertFalse(obj.isEmail("abc@gmail.com.aa.au"));
+    }
 }
